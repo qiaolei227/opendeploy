58d326ad91a7085cd6e7816da310889cb3c6ec4e
david.qiao <286887534@qq.com>
2026-04-19 17:05:45 +0800

    chore: add electron-vite build config

---
+++ electron.vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@main': resolve('src/main'),
+        '@shared': resolve('src/shared')
+      }
+    }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@preload': resolve('src/preload'),
+        '@shared': resolve('src/shared')
+      }
+    }
+  },
+  renderer: {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer'),
+        '@shared': resolve('src/shared')
+      }
+    }
+  }
+});
